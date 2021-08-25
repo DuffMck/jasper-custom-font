5e44273e71f8b41502da3101ec876201e6d56758
DB <d.bonomelli@myti.it>
2021-08-25 22:14:17 +0200

    init Spring Boot project

---
+++ src/main/java/it/softcase/jaspercustomfont/JasperCustomFontApplication.java
@@ -0,0 +1,13 @@
+package it.softcase.jaspercustomfont;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JasperCustomFontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JasperCustomFontApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/softcase/jaspercustomfont/JasperCustomFontApplicationTests.java
@@ -0,0 +1,13 @@
+package it.softcase.jaspercustomfont;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JasperCustomFontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
